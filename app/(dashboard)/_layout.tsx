c496dc413ced6554b1db066a2b1860139cb6fb7c
AchiniPramo <achinipramodhya4@gmail.com>
2025-08-15 11:16:39 +0530

    Install and added icons

--- app/(dashboard)/_layout.tsx
+++ app/(dashboard)/_layout.tsx
@@ -1,6 +1,7 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { Slot, Stack ,Tabs} from "expo-router";
+import {MaterialIcons} from '@expo/vector-icons';
 
 const Layout = () => {
 // ( 
@@ -11,10 +12,10 @@ const Layout = () => {
 //    )
 
  return <Tabs>
-   <Tabs.Screen name = "home" options={{title : "Hello"}}/>
-   <Tabs.Screen name = "tasks" />
-   <Tabs.Screen name = "profile" />
-   <Tabs.Screen name = "setting" />
+   <Tabs.Screen name = "home" options={{title : "Home", tabBarIcon: (data) => <MaterialIcons name="home-filled" size={data.size} color={data.color} />}}/>
+   <Tabs.Screen name = "tasks/index" options={{title : "Task", tabBarIcon: (data) => <MaterialIcons name="check-circle" size={data.size} color={data.color} />}} />
+   <Tabs.Screen name = "profile" options={{title : "Profile", tabBarIcon: (data) => <MaterialIcons name="person" size={data.size} color={data.color} />}} />
+   <Tabs.Screen name = "setting" options={{title : "Setting", tabBarIcon: (data) =><MaterialIcons name="settings" size={data.size} color={data.color} />}} />
      
 
    </Tabs>
